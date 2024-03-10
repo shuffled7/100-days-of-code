fac27aa7d1ddef1a3cb4a86f3b1e5ef13e52505f
shuffled7 <saadmulla2503@gmail.com>
2024-03-10 13:28:21 +0530

    Program to reverse an array

---
+++ recursion/reverse_array.py
@@ -0,0 +1,40 @@
+def reverse_array(input_array,length,start_index):
+
+    #Base case
+    if (start_index >= length//2):
+        return input_array
+    
+    #swap the values
+    input_array[start_index],input_array[length-start_index-1] = input_array[length-start_index-1],input_array[start_index]
+
+    return reverse_array(input_array,length,start_index+1) 
+    
+def main():
+    length = int(input("Enter the length of array: "))
+    print("Enter values into array:")
+    input_array = [int(input(f"{i+1} of {length}: ")) for i in range(length)]
+    print("Reversed Array:",reverse_array(input_array,length,0))
+
+main()
+'''
+#Using parameterised recursion
+#Taking parameters l=left most index & r=right most index
+def reverse_array(input_array,length,l,r):
+
+    #Base case
+    if (l >= r):
+        return input_array
+    
+    #swap the values
+    input_array[l],input_array[r] = input_array[r],input_array[l]
+
+    return reverse_array(input_array,length,l+1,r-1) 
+    
+def main():
+    length = int(input("Enter the length of array: "))
+    print("Enter values into array:")
+    input_array = [int(input(f"{i+1} of {length}: ")) for i in range(length)]
+    print("Reversed Array:",reverse_array(input_array,length,0,length-1))
+    
+main()
+'''
